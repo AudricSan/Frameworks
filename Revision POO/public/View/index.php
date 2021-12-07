9833f340513a5804a80de5a402d452563b69a6bc
Audric_San <audricrosier@gmail.com>
2021-12-07 17:05:50 +0100

    Update index.php

--- Revision POO/public/View/index.php
+++ Revision POO/public/View/index.php
@@ -5,8 +5,12 @@ echo '<h1> hello world </h1>';
 include '../../private/Model/Class/Car.php';
 include '../../private/Model/DAO/CarDAO.php';
 
-// $peugo106 = new Car(1, '1-HRS-544', 500 ,'red');
-// var_dump($peugo106);
+include '../../private/Model/Class/seller.php';
+include '../../private/Model/DAO/sellerDAO.php';
+
+
+$peugo106 = new Car(1, '1-HRS-544', 500 ,'red');
+var_dump($peugo106);
 
 $DAO = new carDAO;
 
@@ -20,18 +24,34 @@ var_dump($cars);
 
 echo "<h2> INSERT </h2>";
 $new = array('Voiture_Chassis' => '1-JDE-544', 'Voiture_Puissance' => 9000, 'Voiture_Couleur' => 'Rouge');
-// $new2 = array('Voiture_ID' => 1, '1-JDE-544', 'Voiture_Puissance' => 9000, 'Voiture_Couleur' => 'Rouge');
-// $new3 = array('1-JDE-544', 9000, 'Rouge');
-
 $newCar = $DAO->insert($new);
 var_dump($newCar);
 
-
 echo "<h2> UPDATE </h2>";
 $up = array('Voiture_Chassis' => '1-JDE-568', 'Voiture_Puissance' => 1, 'Voiture_Couleur' => 'blue');
-// $new2 = array('Voiture_ID' => 1, '1-JDE-544', 'Voiture_Puissance' => 9000, 'Voiture_Couleur' => 'Rouge');
-// $new3 = array('1-JDE-544', 9000, 'Rouge');
-
 $update = $DAO->update(10, $up);
 var_dump($update);
+
+echo "<h1> SELLER </h1>";
+
+$DAO2 = new sellerDAO;
+
+echo "<h2> FETCH SELLER </h2>";
+$seller = $DAO2->fetch(1);
+var_dump($seller);
+
+echo "<h2> FETCH ALL SELLER </h2>";
+$sellers = $DAO2->fetchAll();
+var_dump($sellers);
+
+echo "<h2> INSERT SELLER </h2>";
+$new = array('Vendeur_Name' => 'Windows', 'Vendeur_FirstName' => 'Linux', 'Vendeur_Bday' => '2021-12-24');
+$newSeller = $DAO2->insert($new);
+var_dump($newSeller);
+
+echo "<h2> UPDATE SELLER </h2>";
+$up = array('Vendeur_Name' => 'Audric', 'Vendeur_FirstName' => 'Rosier', 'Vendeur_Bday' => '2021-12-24');
+$update = $DAO2->update(2, $up);
+var_dump($update);
+
 ?>
