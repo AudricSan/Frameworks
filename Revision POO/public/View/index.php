88ef5454abff732e4e8811da8591466fa04448f8
Audric_San <audricrosier@gmail.com>
2021-12-07 14:48:06 +0100

    Update index.php

--- Revision POO/public/View/index.php
+++ Revision POO/public/View/index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-echo 'hello world';
+echo '<h1> hello world </h1>';
 include '../../private/Model/Class/Car.php';
 include '../../private/Model/DAO/CarDAO.php';
 
@@ -9,10 +9,21 @@ include '../../private/Model/DAO/CarDAO.php';
 // var_dump($peugo106);
 
 $DAO = new carDAO;
-var_dump($DAO->fetch(1));
 
-$DAO2 = new carDAO;
-var_dump($DAO2->fetchAll());
+echo "<h2> FETCH </h2>";
+$car = $DAO->fetch(1);
+var_dump($car);
 
+echo "<h2> FETCH ALL </h2>";
+$cars = $DAO->fetchAll();
+var_dump($cars);
+
+echo "<h2> INSERT </h2>";
+$new = array('Voiture_Chassis' => '1-JDE-544', 'Voiture_Puissance' => 9000, 'Voiture_Couleur' => 'Rouge');
+// $new2 = array('Voiture_ID' => 1, '1-JDE-544', 'Voiture_Puissance' => 9000, 'Voiture_Couleur' => 'Rouge');
+// $new3 = array('1-JDE-544', 9000, 'Rouge');
+
+$newCar = $DAO->insert($new);
+var_dump($newCar);
 
 ?>
