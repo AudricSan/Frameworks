79120d726a8c95f0a140c5389b35e6131d7b7fed
Audric_San <audricrosier@gmail.com>
2021-12-11 16:33:54 +0100

    Create interfaceController.php

---
+++ Revision POO/Controller/interfaceController.php
@@ -0,0 +1,8 @@
+<?php
+interface interfaceController {     
+    public function index ();
+    public function show ($id);
+    public function create ();
+    public function insert($data);
+}
+?>
